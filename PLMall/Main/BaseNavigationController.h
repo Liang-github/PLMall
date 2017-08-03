b4b3e57d3fb8fcea006dd66b7d714e2959dfae00
LIANG <pl941227@icloud.com>
2017-08-03 15:42:41 +0800

    创建导航和tabBar

---
+++ PLMall/Main/BaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  BaseNavigationController.h
+//  PLMall
+//
+//  Created by PengLiang on 2017/8/3.
+//  Copyright © 2017年 PengLiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseNavigationController : UINavigationController
+
+@end
---
+++ PLMall/Main/BaseTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  BaseTabBarController.h
+//  PLMall
+//
+//  Created by PengLiang on 2017/8/3.
+//  Copyright © 2017年 PengLiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTabBarController : UITabBarController
+
+@end
